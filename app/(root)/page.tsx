f512a1c7473c1ff4843a378a7a8743d44b0c4493
bookkuun <bookkuun@gmail.com>
2025-03-31 22:42:13 +0900

    loaderとnot-founeページの実装

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
-const Homepage = () => {
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const Homepage = async () => {
+  await delay(1000);
   return <>Prostore</>;
 };
 
