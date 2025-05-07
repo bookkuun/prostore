d150c028e8a762e607435031f4a88e66ead06bdb
bookkuun <bookkuun@gmail.com>
2025-05-07 21:14:08 +0900

    Zod検証と型推論

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,3 +8,8 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+export function formatNumberWithDecimal(num: number): string {
+  const [int, demical] = num.toString().split(".");
+  return demical ? `${int}.${demical.padEnd(2, "0")}` : `${int}.00`;
+}
